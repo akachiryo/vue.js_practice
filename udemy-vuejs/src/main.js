c38b674ce46661a2764875bc0e3d954697ee4c32
akachiryo <rirugameri@icloud.com>
2021-10-22 00:57:18 +0900

    component

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import LikeNumber from './LikeNumber.vue'
+
+const app = Vue.createApp({})
+app.component('LikeNumber', LikeNumber);
 
 createApp(App).mount('#app')
---
+++ udemy-vuejs2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+ <like-header></like-header>
+ <LikeNumber></LikeNumber>
+</div>
+</template>
+
+<script>
+import LikeHeader from './components/LikeHeader.vue'
+export default {
+  components: {
+    LikeHeader: LikeHeader
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
