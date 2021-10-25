187691294f62e8a8919817c8390423fb6b35554d
akachiryo <rirugameri@icloud.com>
2021-10-25 17:18:29 +0900

    animation finish

--- animation-vuejs/src/App.vue
+++ animation-vuejs/src/App.vue
@@ -1,28 +1,186 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="main">
+  <button @click="myAnimation ='slide'">Slide</button>
+  <button @click="myAnimation ='fade'">Fade</button>
+  <p>{{ myAnimation }}</p>
+  <br>
+  <button @click="add">追加</button>
+  <ul style="width: 200px; margin: auto;">
+    <transition-group name="fade"> 
+      <li 
+      style="cursor: pointer;"
+      v-for="number, index in numbers"
+      @click="remove(index)"
+      :key="number"
+      >{{ number }}</li>
+    </transition-group>
+  </ul>
+  <br>
+  <button v-on:click="show = !show">切り替えボタン</button>
+  <br><br>
+  <transition
+  :css="false"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @enter-cancelled="enterCancelled"
+
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    @leave-cancelled="leaveCancelled"
+  >
+    <div class="circle" v-if="show"></div>
+  </transition>
+  <br>
+  <button @click="myComponent = 'ComponentA'">ComponentA</button>
+  <button @click="myComponent = 'ComponentB'">ComponentB</button>
+  <transition name="fade" mode="out-in">
+    <component :is="myComponent"></component>
+  </transition>
+  <transition name="fade" mode="out-in">
+    <p v-if="show" key="bye">さようなら</p>
+    <p v-else key="hello">こんにちは</p>
+  </transition>
+  <transition 
+  enter-active-class="animated bounce"
+  leave-active-class="animated shake"
+  appear>
+    <p v-if="show">Hello</p>
+  </transition>
+  <transition :name="myAnimaition" appear>
+    <p v-if="show">bye</p>
+  </transition>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComponentA from './components/ComponentA.vue'
+import ComponentB from './components/ComponentB.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ComponentA,
+    ComponentB
+  },
+  data() {
+    return {
+      show: true,
+      myAnimation: "slide",
+      myComponent: "ComponentA",
+      numbers: [0,1,2],
+      nextNumber: 3
+    };
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber)
+      this.nextNumber += 1;
+    },
+      remove(index) {
+        this.numbers.splice(index, 1);
+      },
+    beforeEnter(el) {
+      el.style.transform = 'scale(0)'
+    },
+    enter(el, done) {
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform =`scale(${scale})`;
+        scale += 0.1;
+        if ( scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    // afterEnter(el) {},
+    // enterCancelled(el) {},
+    // beforeLeave(el) {},
+    leave(el, done) {
+       let scale = 1;
+        const interval = setInterval(() => {
+        el.style.transform =`scale(${scale})`;
+        scale -= 0.1;
+        if ( scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    }
+    // afterLeave(el) {},
+    // leaveCancelled(el) {}
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: pink;
 }
+
+.fade-move {
+  transition: transform 1s;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 0.5s;
+  position: absolute;
+  width: 200px;
+}
+.fade-leave-to {
+opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+
+
+.slide-enter-active {
+  animation: slide-in 0.5s;
+  transition: 0.5s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+  transition: 0.5s;
+}
+
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+
 </style>
